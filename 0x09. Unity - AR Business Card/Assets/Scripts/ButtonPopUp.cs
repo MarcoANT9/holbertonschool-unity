5c468aba091ec50fae5890cff1c4da47be8e96b8
MarcoAnt9 <acalde27@hotmail.com>
2021-04-06 18:24:02 -0500

    Added Buttons, functionality and animations

---
+++ 0x09. Unity - AR Business Card/Assets/Scripts/ButtonPopUp.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class ButtonPopUp : MonoBehaviour, ITrackableEventHandler
+{
+
+    //================================================================================
+    // Public Variables ==============================================================
+    //================================================================================
+
+    //================================================================================
+    // Private Variables =============================================================
+    //================================================================================
+    private TrackableBehaviour mTrackableBehavior;
+
+    //================================================================================
+    // Start is called before the first frame update =================================
+    //================================================================================
+    void Start()
+    {
+        mTrackableBehavior = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehavior)
+            mTrackableBehavior.RegisterTrackableEventHandler(this);
+
+    }
+
+    //================================================================================
+    // Update is called once per frame ===============================================
+    //================================================================================
+    void Update()
+    {
+    }
+
+    //================================================================================
+    // Coroutines ====================================================================
+    //================================================================================
+
+
+    //================================================================================    
+    // Functions =====================================================================
+    //================================================================================
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus
+    )
+    {
+        Animator[] animationClips = GetComponentsInChildren<Animator>();
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+        newStatus == TrackableBehaviour.Status.TRACKED ||
+        newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            foreach (Animator component in animationClips)
+				component.enabled = true;
+        }
+
+        else
+        {
+            foreach (Animator component in animationClips)
+            {
+				component.Rebind();
+				component.enabled = false;
+            }
+        }
+    }
+
+}
---
+++ 0x09. Unity - AR Business Card/Assets/Scripts/LinkFunctions.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class LinkFunctions : MonoBehaviour
+{
+	void Start()
+	{
+		
+
+	}
+	void Update()
+	{
+		
+	}
+
+	//================================================================================    
+	// Functions =====================================================================
+	//================================================================================
+
+	public void GithubApp()
+	{
+		Application.OpenURL("https://github.com/MarcoANT9/");
+	}
+
+	public void MediumApp()
+	{
+		Application.OpenURL("https://medium.com/@acalde27");
+	}
+
+	public void LinkedInApp()
+	{
+		Application.OpenURL("https://co.linkedin.com/in/marcoant91");
+	}
+
+	public void TwitterApp()
+	{
+		Application.OpenURL("https://github.com/MarcoANT9/");
+	}
+}
